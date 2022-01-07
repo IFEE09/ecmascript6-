a89ead43e9b74ccb206948ab9891322ba7e102f2
IFEE09 <ivanfaridespadas@gmail.com>
2022-01-07 14:08:19 -0600

    adding template literals and functions with ES6

--- src/es6/index.js
+++ src/es6/index.js
@@ -1,5 +1,25 @@
+//Normal function in js
 function newFunction(name, age, country) {
     var name = name || 'ivan';
     var age = age || 29;
     var country = country || 'MX';
+    console.log(name, age, country);
 }
+
+//ES6
+function newFunction2(name = 'Ivan', age = '29', country = 'MX') {
+    console.log(name, age, country);
+}
+
+//Running ES6
+newFunction2();
+newFunction2('Richard', '25', 'USA' )
+
+//Normal form in JS
+let hello = 'Hello';
+let world = 'World';
+let epicPhrase = hello + ' ' + world;
+console.log(epicPhrase);
+
+//Template literals in ES6
+let epicPhrase = `${hello} ${world}`;
