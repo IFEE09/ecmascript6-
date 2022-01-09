3cb2973706537a760b39af3eb4f3f90af8e3f9a9
IFEE09 <ivanfaridespadas@gmail.com>
2022-01-08 20:50:17 -0600

    adding new features of ES11

--- src/es11/index.js
+++ src/es11/index.js
@@ -10,3 +10,31 @@ const aBigNumber = 9007199254740991n;
 
 //The same of the Big Int
 const anotherBigNumber = BigInt(9007199254740991);
+
+//promises allsettled
+const promise1 = new Promise((resolve, reject) => reject("reject"));
+const promise2 = new Promise((resolve, reject) => resolve("resolve"));
+const promise3 = new Promise((resolve, reject) => resolve("resolve 1"));
+
+Promise.allSettled([promise1, promise2, promise3])
+    .then(response => console.log(response));
+
+//global this
+console.log(window);
+console.log(globalThis);
+
+//Run this on console of the browser
+//Ternary operator
+const fooo = null ?? 'default string';
+console.log(fooo);
+
+//Run this on console of the browser
+//Optional Chaining
+const user = {};
+console.log(user?.profile?.email);
+
+if(user?.profile?.email) {
+    console.log('email');
+} else {
+    console.log('fail');
+}
